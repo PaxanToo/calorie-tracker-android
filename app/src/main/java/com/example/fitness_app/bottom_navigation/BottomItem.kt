cddc86e3878b6262eb688a2776fd771b570b07f1
Павел Игнатов <paxan2005i@gmail.com>
2025-12-16 19:44:09 +0300

    Работа за 16 декабря  сделан прототип панели навигации между окнами

---
+++ app/src/main/java/com/example/fitness_app/bottom_navigation/BottomItem.kt
@@ -0,0 +1,30 @@
+package com.example.fitness_app.bottom_navigation
+
+import com.example.fitness_app.R
+
+sealed interface BottomItem {
+    val title: String
+    val iconId: Int
+    val route: String
+}
+
+data object Home : BottomItem {
+    override val title = "Главная"
+    override val iconId = R.drawable.icon1
+    override val route = "home"
+}
+data object Page2 : BottomItem {
+    override val title = "Страница 2"
+    override val iconId = R.drawable.icon2
+    override val route = "page2"
+}
+data object Page3 : BottomItem {
+    override val title = "Страница 3"
+    override val iconId = R.drawable.icon1
+    override val route = "page3"
+}
+data object Page4 : BottomItem {
+    override val title = "Страница 4"
+    override val iconId = R.drawable.icon1
+    override val route = "page4"
+}
