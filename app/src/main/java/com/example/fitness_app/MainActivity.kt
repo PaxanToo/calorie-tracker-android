88d5a205f804278471da726b8de936471d0484f0
Павел Игнатов <paxan2005i@gmail.com>
2025-12-16 12:10:22 +0300

    Пример создания карточки с картинкой

--- app/src/main/java/com/example/fitness_app/MainActivity.kt
+++ app/src/main/java/com/example/fitness_app/MainActivity.kt
@@ -4,64 +4,78 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.fitness_app.ui.theme.Fitness_appTheme
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           /* Column{ // можно делать контейнеры как и лейауты
-           // с помошью mofifier можно по разному их распологать и занимать есто на экране
-                Text(text = "Hello")
-                Text(text = "Heasdllo")
-                Text(text = "Hello123")
-                Text(text = "Hello")
-                Text(text = "Hsdasello222222")
-            }
-         */
-          /*  Row {
-                Text(text = "Hello")
-                Text(text = "Heasdllo")
-                Text(text = "Hello123")
-                Text(text = "Hello")
-                Text(text = "Hsdasello222222")фывфыв
-            } */
-
-            /*Row(
-                modifier = Modifier.
-            background(Color.Red).fillMaxSize()
-            )
-            {
-                Text(text = "Hello")
-                Text(text = "Heasdllo")
-                Text(text = "Hello123")
-                Text(text = "Hello")
-                Text(text = "Hsdasello222222")
-            }
-             */
-
-
-
-
-
-
-
+            ListItem("Енот","Мусор")
 
             }
         }
     }
 
+@Composable
+private fun ListItem(name: String, Bfood: String){
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Gray)
+    ){
+       /* Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Text(text = "test")
+
+        }
+        */
+
+        Box(){
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Image(
+                    painter = painterResource(id = R.drawable.image_raccon),
+                    contentDescription = "картинка",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.padding(15.dp).size(64.dp).clip(CircleShape)
+
+                )
+
+                Column(){
+                    Text(text = name)
+                    Text(text = Bfood)
+                }
+
+            }
+        }
+    }
+
+}
 
 
 
