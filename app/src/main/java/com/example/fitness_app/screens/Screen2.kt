18cf39c6dab222dba76b6386d9eaa8c4eff24764
Павел Игнатов <paxan2005i@gmail.com>
2025-12-17 20:20:29 +0300

    пробный функционал, без сохранения

--- app/src/main/java/com/example/fitness_app/screens/Screen2.kt
+++ app/src/main/java/com/example/fitness_app/screens/Screen2.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 
 @Preview(showBackground = true)
@@ -24,6 +26,24 @@ fun Screen2Preview() {
 
 @Composable
 fun Screen222() {
+
+
+    val height = remember { mutableStateOf("") }
+    val weight = remember { mutableStateOf("") }
+    val result = remember { mutableStateOf("") }
+
+
+
+
+
+
+
+
+
+
+
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -31,35 +51,49 @@ fun Screen222() {
         verticalArrangement = Arrangement.Center
     ) {
 
-        Text(
-            text = "Введите данные",
-            fontSize = 20.sp
-        )
-
+        Text("Введите данные", fontSize = 20.sp)
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Рост") }
-        )
+        Text(text = result.value, fontSize = 16.sp)
+        Spacer(modifier = Modifier.height(16.dp))
 
+
+
+        OutlinedTextField(
+            value = height.value,
+            onValueChange = { height.value = it },
+            label = { Text("Рост (см)") }
+        )
         Spacer(modifier = Modifier.height(8.dp))
 
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Вес") }
+            value = weight.value,
+            onValueChange = { weight.value = it },
+            label = { Text("Вес (кг)") }
         )
-
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
-            onClick = {
-                // тут потом будет сохранение в БД
+        Button(onClick = {
+            val heightCm = height.value.toFloatOrNull()
+            val weightKg = weight.value.toFloatOrNull()
+
+            if (heightCm != null && weightKg != null && heightCm > 0) {
+                val heightM = heightCm / 100
+                val bmi = weightKg / (heightM * heightM)
+
+                result.value =
+                    "Рост: ${height.value} см\n" +
+                            "Вес: ${weight.value} кг\n" +
+                            "ИМТ: ${"%.2f".format(bmi)}"
+            } else {
+                result.value = "Введите корректные значения"
             }
-        ) {
+        }) {
             Text("Сохранить")
         }
     }
-    }
+}
+
+
+
+
