e6b4d1bf8a7ec25f5ed8da69fb4070faa62a8d98
Павел Игнатов <paxan2005i@gmail.com>
2025-12-19 18:55:49 +0300

    нашёл на гит хабе анимации

--- app/src/main/java/com/example/fitness_app/MainActivity.kt
+++ app/src/main/java/com/example/fitness_app/MainActivity.kt
@@ -1,83 +1,27 @@
-package com.example.fitness_app
+package com.juraj.fluid
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.fitness_app.ui.theme.Fitness_appTheme
-import androidx.compose.material3.CardDefaults
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
+import com.juraj.fluid.ui.theme.FluidBottomNavigationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                ButtonItem()
-
-            }
-
-
+            FluidBottomNavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
             }
         }
     }
-
-@Composable
-private fun ButtonItem() {
-    val counter = remember { mutableStateOf(0) }
-    val color = remember { mutableStateOf(Color.Black) }
-
-
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .background(color = color.value, shape = CircleShape).clickable {
-                counter.value
-                when(++counter.value){
-                    10 -> color.value = Color.Yellow
-                }
-            },
-            contentAlignment = Alignment.Center
-    ) {
-
-        Text(text = counter.value.toString(), style = TextStyle(color = Color.White, fontSize = 20.sp))
-
-    }
 }
-
-
-
-
-
